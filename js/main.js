0fe9999955d336b681e872b17798730309e302f2
albinbn <albinbn@hotmail.com>
2017-02-11 19:18:33 +0100

    adfda
    adfdf

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $("#favourit").hover(function() {
+        $('#favid').attr('src', 'images/favourit-final.png');
+            }, function() {
+     $('#favid').attr('src', 'images/favourit-farg.png');
+    });
+        
+    });
+
+$(document).ready(function() {
+    $("#shap").hover(function() {
+        $('#shapimg').attr('src', 'images/shapy_logga_vit.png');
+            }, function() {
+     $('#shapimg').attr('src', 'images/shapy_logga_sv.png');
+    });
+        
+    });
+
+$(document).ready(function() {
+    console.log("pinkdick");
+    $("#zoe").hover(function() {
+        $('#zimageid').attr('src', 'images/Zoega_vit.png');
+            }, function() {
+     $('#zimageid').attr('src', 'images/Zoe%C2%A6%C3%BCgas_logo_black.png');
+    });
+        
+    });
+
+$(document).ready(function() {
+    $("#hels").hover(function() {
+        $('#helsimg').attr('src', 'images/helsingbäst_vit.png');
+            }, function() {
+     $('#helsimg').attr('src', 'images/Helsingbäst_svart.png');
+    });
+        
+    });
+
+$(document).ready(function() {
+    $("#fil").hover(function() {
+        $('#filimg').attr('src', 'images/Filippa_vit3.png');
+            }, function() {
+     $('#filimg').attr('src', 'images/Filippa_svart.png');
+    });
+        
+    });
+
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
